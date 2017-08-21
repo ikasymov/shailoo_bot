190b03725afb066fcba3ff8d8081d51176245951
ilgizkasymov <ilgizkasymov@gmail.com>
2017-08-21 11:41:26 +0600

    add handler logic

---
+++ handler.js
@@ -0,0 +1,39 @@
+let request = require('request');
+let apiUrl = 'https://namba1.co/api/';
+let token = process.env.token;
+
+function Handler(req){
+    this.data = req.body.data;
+    this[req.body.event]()
+}
+
+Handler.prototype.newMessage = async function(){
+    this.message = this.data.content;
+    this.chat_id = this.data.chat_id;
+    return await this.sendMessage('Hello world')
+};
+
+Handler.prototype.sendMessage = async function(message){
+    const data = {
+        url: apiUrl + '/chats/' + this.chat_id + '/write',
+        method: 'POST',
+        body: {
+            type: 'text/plain',
+            content: message
+        },
+        headers: {
+            'X-Namba-Auth-Token': token
+        },
+        json: true
+    };
+    return new Promise((resolve, reject)=>{
+        request(data, (error, req, body)=>{
+            if(error){
+                reject(error)
+            }
+            resolve(body)
+        })
+    })
+};
+
+module.exports = Handler;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let Parser = require('../handler');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+    new Parser(req);
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
